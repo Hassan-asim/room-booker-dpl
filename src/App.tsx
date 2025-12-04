6d1326a75f2994ea4ad31c89ad2ad39a2895c350
Sufi Hassan Asim <hassan.u@dplit.com>
2025-12-04 14:54:57 +0500

    fix: Resolve useLocation() error by moving BrowserRouter to main.tsx and updating App.tsx accordingly.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"; // Import useLocation
+import { Routes, Route, useLocation } from "react-router-dom"; // Updated import
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import MyBookings from "./pages/MyBookings";
@@ -12,8 +12,8 @@ import NotFound from "./pages/NotFound";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import { ThemeProvider } from "./components/theme-provider";
 import SplashScreen from './components/SplashScreen';
-import RoomStatusOverlay from './components/RoomStatusOverlay'; // Import RoomStatusOverlay
-import useIdleTimer from './hooks/useIdleTimer'; // Import useIdleTimer
+import RoomStatusOverlay from './components/RoomStatusOverlay';
+import useIdleTimer from './hooks/useIdleTimer';
 
 const queryClient = new QueryClient();
 
@@ -53,7 +53,7 @@ const App = () => {
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <> {/* Fragment instead of BrowserRouter */}
             {showOverlay && <RoomStatusOverlay onClick={dismissOverlay} />}
             <Routes>
               <Route path="/" element={<Index />} />
@@ -65,7 +65,7 @@ const App = () => {
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </BrowserRouter>
+          </>
         </TooltipProvider>
       </ThemeProvider>
     </QueryClientProvider>
